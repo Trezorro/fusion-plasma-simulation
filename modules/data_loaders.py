f4cd14b834a9484a7ab604f7123e041cf1f2a2bf
Milan Tresoor <milan.tresoor@gmail.com>
2024-07-09 10:57:12 +0200

    Add optional random start (-end) to data loader get sample, still no training progress

--- modules/data_loaders.py
+++ modules/data_loaders.py
@@ -2,28 +2,42 @@ import pandas as pd
 import wandb
 import torch
 from torch.utils import data
+import random
 
 
 class MyDataset(data.Dataset):
 
-    def __init__(self, file_path, columns_C, columns_X):
-        self.data = pd.read_parquet(file_path)
-        self.shot_numbers = self.data['ShotNum'].unique()
+    def __init__(self, file_path, columns_C, columns_X, seq_length=2000, crop_margin=1000, random_start=True):
+        self.file_path = file_path
         self.columns_C = columns_C
         self.columns_X = columns_X
+        self.seq_length = seq_length
+        self.crop_margin = crop_margin
+        self.random_start = random_start
+
+        self.data = pd.read_parquet(file_path)
+        self.shot_numbers = self.data['ShotNum'].unique()
         self.min = self.data[columns_C + columns_X].min()
         self.max = self.data[columns_C + columns_X].max()
-        self.data[columns_C + columns_X] = (self.data[columns_C + columns_X] - self.min) / (self.max - self.min)
+        self.data[columns_C +
+                  columns_X] = (self.data[columns_C + columns_X] - self.min) / (self.max - self.min)
 
     def __len__(self):
         return len(self.shot_numbers)
 
     def __getitem__(self, idx):
         shot_number = self.shot_numbers[idx]
-        shot_data = self.data[self.data['ShotNum'] == shot_number] # indexed by time
-        C = shot_data[self.columns_C].iloc[1000:3000].values
-        X = shot_data[self.columns_X].iloc[1000:3000].values
-        return shot_number, C, X # C and X shapes: (seq_length, variables)
+        shot_data = self.data[self.data['ShotNum'] == shot_number]  # indexed by time
+        shot_len = len(shot_data)  # Should be at minumum 5000 based on preprocessing
+        viable_start_max = shot_len - self.crop_margin - self.seq_length
+        assert viable_start_max > self.crop_margin, (
+            f"Shot {shot_number} is too short (T{shot_len}) for desired "
+            f"seq_length {self.seq_length} and crop_margin {self.crop_margin}")
+        start = random.randint(self.crop_margin, viable_start_max) if self.random_start else self.crop_margin
+        end = start + self.seq_length
+        C = shot_data[self.columns_C].iloc[start:end].values
+        X = shot_data[self.columns_X].iloc[start:end].values
+        return shot_number, C, X  # C and X shapes: (seq_length, variables)
 
 
 def slice_data(data, start_time, end_time):
@@ -35,4 +49,3 @@ def slice_data(data, start_time, end_time):
 #     sig[ALL_SIG_COLLS] = (sig[ALL_SIG_COLLS] -
 #                         sig[ALL_SIG_COLLS].mean()) / sig[ALL_SIG_COLLS].std()
 #     return (data - data.mean()) / data.std()
-
--- run.py
+++ run.py
@@ -49,11 +49,15 @@ data_set = MyDataset(
     file_path=C.data.dir + C.data.file,
     columns_C=list(C.data.cols.c),
     columns_X=list(C.data.cols.x),
+    seq_length=C.seq_length,
+    crop_margin=C.crop_margin,
+    random_start=C.random_start,
 )
 train_set, val_set = data.random_split(data_set, [0.9, 0.1], generator=torch.Generator().manual_seed(42))
 
 train_loader = data.DataLoader(train_set, batch_size=C.batch_size, shuffle=True)
-val_loader = data.DataLoader(val_set, batch_size=C.batch_size, shuffle=False)
+val_loader = data.DataLoader(val_set, batch_size=C.batch_size * 2, shuffle=False)
+val_loader.dataset.random_start = False
 
 
 def validate(model, data_loader, criterion):
