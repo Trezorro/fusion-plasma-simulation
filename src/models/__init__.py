1364b6e21216fb68d0ede22c0c095d0f3e33962b
Milan Tresoor <milan.tresoor@gmail.com>
2024-10-09 14:15:19 +0200

    Fix capitalization

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 from src.models.CausalConv1d import AutoRegressiveModel
 from src.models.EncoderDecoder import EncoderDecoder
-from src.models.UNet import UNet
+from src.models.unet import UNet
 from src.models.UNet_fourier import UNetFourier
 from src.models.complexnet import ComplexNet
