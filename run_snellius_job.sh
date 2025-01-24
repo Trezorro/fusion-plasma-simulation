fc4527e200a853bfbf6c92d1f18fd097079a9f98
Milan Tresoor <milan.tresoor@gmail.com>
2025-01-24 15:32:53 +0100

    Fix missing matplotlib and set time to 10 mins

--- run_snellius_job.sh
+++ run_snellius_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=output/slurms/agent-%j.out
 #SBATCH --error=output/slurms/agent-%j.out
 #SBATCH --partition=gpu_mig
-#SBATCH --time=60:00
+#SBATCH --time=10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -27,6 +27,7 @@ echo "Loading modules..."
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 module load SciPy-bundle/2023.07-gfbf-2023a  # has beniget-0.4.1, Bottleneck-1.3.7, deap-1.4.0, gast-0.5.4, mpmath-1.3.0, numexpr-2.8.4, numpy-1.25.1, pandas-2.0.3, ply-3.11, pythran-0.13.1, scipy-1.11.1, tzdata-2023.3, versioneer-0.29
+module load matplotlib/3.7.2-gfbf-2023a # vs 3.9.3
 module load PyTorch/2.1.2-foss-2023a # vs my installed version 2.2.2
 module load plotly.py/5.16.0-GCCcore-12.3.0 # vs 5.24.1
 
