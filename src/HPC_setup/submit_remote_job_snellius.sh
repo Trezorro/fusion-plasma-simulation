c9371c0cc9f71b8d966d5c2847c489e38969f9fb
Milan Tresoor <milan.tresoor@gmail.com>
2025-02-11 14:42:25 +0100

    Add sync slurms task

--- src/HPC_setup/submit_remote_job_snellius.sh
+++ src/HPC_setup/submit_remote_job_snellius.sh
@@ -12,15 +12,23 @@ REMOTE_SLURM_DIR="$REMOTE_USER@$REMOTE_HOST:/home/$REMOTE_USER/fusion-plasma-sim
 LOCAL_HPC_PATH="output/snellius/"
 QUEUE_FORMAT="--format=\"%.18i %.50j %.12u %.8T %.10M %.6D %.10P %.17R\""
 
+sync_slurms() {
+    # Run rsync and capture the output
+    RSYNC_OUTPUT=$(rsync -avzv $REMOTE_SLURM_DIR $LOCAL_HPC_PATH)
+    # Filter and format the output
+    echo "Updated files:"
+    echo "$RSYNC_OUTPUT" | grep '\.out' | grep -v 'is uptodate' | awk -v LOCAL_HPC_PATH="$LOCAL_HPC_PATH" '{print LOCAL_HPC_PATH $1}'
+}
+
 # Check if a job name argument is provided
 if [ "$#" -ne 1 ] || [ -z "$1" ]; then
     echo "To submit a new job, use: $0 <job_name>"
     echo "Will check the queue and sync results from snellius."
     # SSH into the main node, check the queue status, and run rsync
+    sync_slurms
     ssh -T -o LogLevel=ERROR $REMOTE_USER@$REMOTE_HOST << EOF
-        squeue
+        squeue $QUEUE_FORMAT
 EOF
-    rsync -avzv $REMOTE_SLURM_DIR $LOCAL_HPC_PATH
     exit 0
 fi
 
@@ -52,13 +60,7 @@ EOF
 
 echo "Code updated, SLURM job '$JOB_NAME' submitted, and Git tag '$JOB_NAME' created. :D"
 
-sync_slurms() {
-    # Run rsync and capture the output
-    RSYNC_OUTPUT=$(rsync -avzv $REMOTE_SLURM_DIR $LOCAL_HPC_PATH)
-    # Filter and format the output
-    echo "Updated files:"
-    echo "$RSYNC_OUTPUT" | grep '\.out' | grep -v 'is uptodate' | awk -v LOCAL_HPC_PATH="$LOCAL_HPC_PATH" '{print LOCAL_HPC_PATH $1}'
-}
+
 
 sleep 1
 echo "Syncing results from HPC in 10 seconds..."
