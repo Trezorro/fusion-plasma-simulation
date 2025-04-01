3c2d88274a8ceda632829b8ad4774dd7864745bc
Milan Tresoor <milan.tresoor@gmail.com>
2025-04-01 16:07:23 +0200

    remove old plotly from setup and pip install new version

--- run_snellius_sweep_CPU.sh
+++ run_snellius_sweep_CPU.sh
@@ -36,7 +36,6 @@ module load Python/3.11.3-GCCcore-12.3.0
 module load SciPy-bundle/2023.07-gfbf-2023a  # has beniget-0.4.1, Bottleneck-1.3.7, deap-1.4.0, gast-0.5.4, mpmath-1.3.0, numexpr-2.8.4, numpy-1.25.1, pandas-2.0.3, ply-3.11, pythran-0.13.1, scipy-1.11.1, tzdata-2023.3, versioneer-0.29
 module load matplotlib/3.7.2-gfbf-2023a # vs 3.9.3
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1 # vs my installed version 2.2.2
-module load plotly.py/5.16.0-GCCcore-12.3.0 # vs 5.24.1
 
 echo "Activating virtual environment..."
 source ~/fusion/bin/activate
