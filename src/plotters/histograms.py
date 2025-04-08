36aaa7ae1a22fa01557ee250d13345d160799323
Milan Tresoor <milan.tresoor@gmail.com>
2025-04-08 17:32:50 +0200

    Histogram split by peak modes

--- src/plotters/histograms.py
+++ src/plotters/histograms.py
@@ -2,7 +2,7 @@
 
 #%% Imports
 from typing import Callable, List, Literal, Tuple
-import plotly
+import wandb
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
@@ -17,7 +17,9 @@ logger.setLevel(logging.DEBUG)
 #%%
 
 import plotly.graph_objects as go
-COLOR_SCALE = plotly.colors.sequential.Plasma
+# COLOR_SCALE = plotly.colors.sequential.Plasma
+COLOR_SCALE = ['#636EFA', '#00CC96', '#EF553B']
+
 
 def plot_peak_prominences_histogram(
     peak_features: dict,
@@ -41,100 +43,66 @@ def plot_peak_prominences_histogram(
     C = get_current_config()
     channel_idx = C.data.cols.x.index(channel_name)
     history_length = C.data.history_length
-    pred_peaks = peak_features["pred_peaks"]
-    target_peaks = peak_features["target_peaks"]
+    pred_peaks = peak_features["pred_peaks"][channel_idx]
+    target_peaks = peak_features["target_peaks"][channel_idx]
     shot_numbers = meta["shot_number"]
-    labels = conditioning_input['label'].numpy()[:, history_length:] # shape: (n_samples, time steps)
+    labels = conditioning_input['label'].numpy()[:, history_length:]  # shape: (n_samples, time steps)
     shot_label_means = labels.mean(axis=1)
     if len(pred_peaks) > n:
         pred_peaks = pred_peaks[:n]
     if len(target_peaks) > n:
         target_peaks = target_peaks[:n]
 
-    pred_prominences = [sample.prominences for sample in pred_peaks[channel_idx]]
-    target_prominences = [sample.prominences for sample in target_peaks[channel_idx]]
-
     # Prepare data for plotly express
     data = []
-    for shot_num, pred_i, target_i_data, label_mean_i in zip(shot_numbers, pred_prominences, target_prominences, shot_label_means):
-        for prominence in pred_i:
-            data.append({
-                "group": "Predicted",
-                "label": label_mean_i.item(),
-                "prominence_value": prominence,
-                "shot_num": shot_num.item()
-            })
-        for prominence in target_i_data:
-            data.append({
-                "group": "Target",
-                "label": label_mean_i.item(),
-                "prominence_value": prominence,
-                "shot_num": shot_num.item()
-            })
-
+    for shot_i in range(len(shot_numbers)):
+        shot_num = shot_numbers[shot_i].item()
+        for peak in pred_peaks[shot_i]:
+            data.append(
+                {
+                    "group": "Predicted",
+                    "mode": int(labels[shot_i][peak.X].item()),
+                    "prominence": peak.prominences,
+                    "shot_num": shot_num
+                }
+            )
+        for peak in target_peaks[shot_i]:
+            data.append(
+                {
+                    "group": "Target",
+                    "mode": int(labels[shot_i][peak.X].item()),
+                    "prominence": peak.prominences,
+                    "shot_num": shot_num
+                }
+            )
     df = pd.DataFrame(data)
+    # map modes to L, D, H
+    df["mode"] = df["mode"].map({1: "L", 2: "D", 3: "H", 0: "?"})
     # plot px.histogram
     fig = px.histogram(
         df,
-        x="prominence_value",
-        color="shot_num",
+        x="prominence",
+        color="mode",
         barmode="stack",
         facet_col="group",
-        facet_col_wrap=2,
+        # facet_col_wrap=2,
         histnorm="probability density",
         color_discrete_sequence=COLOR_SCALE,
         title=f"{title_base} Histogram of Peak Prominences for Channel: {channel_name}",
-        labels={"prominence_value": "Prominence", "group": "Group", "shot_num": "Shot Number"},
-        hover_data=["shot_num", "label"],
+        labels={
+            "prominence": "Prominence",
+            "group": "Group",
+            "shot_num": "Shot Number"
+        },
+        hover_data=["shot_num", "mode"],
         marginal="rug",
-        # marginal_kws=dict(binsize=0.1),
+        nbins=200,  # Make bins smaller by increasing the number of bins
+        category_orders={"mode": ['L', 'D', 'H', '?']},  # Add order to mode
     )
-    fig.show()
-
-    def add_hist_trace(fig, shot_num, data, label_mean_i, group_name: Literal['Target', 'Predicted']):
-        fig.add_trace(
-            go.Histogram(
-                x=data,
-                name=f"{group_name} for shot {shot_num}",
-                nbinsx=100,
-                opacity=0.75,
-                yaxis="y1" if group_name == "Target" else "y2",
-                histnorm='density',
-                # marker_color='red',
-                marker=dict(
-                    colorscale="Plasma" if group_name == "Target" else "Turbo",
-                    color=(label_mean_i.item(),) * 50,
-                    cmin=1.0,
-                    cmid=2.0,
-                    cmax=3.0,
-                    # colorbar=dict(title="Label Mean"),
-                    # coloraxis="coloraxis",
-                ),
-                text=shot_num.item(),
-                customdata=(label_mean_i.item(),) * 50,
-                legendgroup=f'{group_name}',
-                legendgrouptitle_text=f"{group_name}",
-                hovertemplate=group_name + " prominence: %{x}<br>Count: %{y}<br>Label: %{customdata}<extra></extra>",
-                hoverinfo="x+y",
-                showlegend=True,
-                bingroup=1
-            )
-        )
-    fig = go.Figure()
-    # Add predicted prominences
-    for shot_num, pred_i, target_i_data, label_mean_i in zip(shot_numbers, pred_prominences, target_prominences, shot_label_means):
-        add_hist_trace(fig, shot_num, pred_i, label_mean_i, "Predicted")
-        add_hist_trace(fig, shot_num, target_i_data, label_mean_i, "Target")
-
-    fig.update_layout(
-        title=f"{title_base} Histogram of Peak Prominences for Channel: {channel_name}",
-        xaxis_title="Prominence",
-        yaxis_title="Probability",
-        yaxis=dict(title="Target Probability"),
-        yaxis2=dict(title="Predicted Probability", overlaying="y", side="right"),
-        barmode="stack",
-        legend_title="Distributions",
-    )
-    fig.show()
-
-
+    # fig.update_layout(
+    #     hovermode="x unified",
+    #     xaxis=dict(matches='x'),  # Link x-axis hovering between facets
+    # )
+    if wandb.run.disabled:  # type: ignore
+        fig.show()
+    return fig
