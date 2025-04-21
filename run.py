83c403ffb68ba3e08158dd68bbc3b0b5d80861e1
Milan Tresoor <milan.tresoor@gmail.com>
2025-04-21 14:05:57 +0200

    Test logging artifacts

--- run.py
+++ run.py
@@ -20,8 +20,20 @@ run = wandb.init(
 )
 C = get_current_config()
 RUN_NAME = wandb.run.name
-logger.info("Run initialized, importing torch and lightning.")
+# Save the configuration to a pickle file
+import pickle
 
+logger.info("Saving configuration to pickle file.")
+config_pickle_path = "output/config.pkl"
+with open(config_pickle_path, "wb") as f:
+    pickle.dump(conf, f)
+logger.info("Creating W&B artifact for configuration.")
+# Upload the configuration pickle file as a W&B artifact
+config_artifact = wandb.Artifact("config", type="configuration")
+config_artifact.add_file(config_pickle_path)
+wandb.log_artifact(config_artifact)
+logger.info("Configuration saved and artifact created.")
+logger.info("Run initialized, importing torch and lightning.")
 import torch
 from torch.utils import data
 import lightning as L
@@ -29,7 +41,7 @@ import lightning.pytorch.callbacks as pl_callbacks
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import DeviceStatsMonitor
 
-torch.cuda.memory._record_memory_history()
+# torch.cuda.memory._record_memory_history()
 
 logger.info("Torch and lightning imported, importing src modules.")
 
@@ -72,13 +84,25 @@ val_loader = data.DataLoader(
 )
 
 logger.info("Data loaded, initializing trainer.")
+# torch.profiler.profile(
+#     activities=[
+#         torch.profiler.ProfilerActivity.CPU,
+#         torch.profiler.ProfilerActivity.CUDA],
+#     schedule=torch.profiler.schedule(
+#         wait=1,
+#         warmup=1,
+#         active=10),
+#     on_trace_ready=torch.profiler.tensorboard_trace_handler(logdir, worker_name='worker0'),
+#     record_shapes=True,
+#     profile_memory=True,  # This will take 1 to 2 minutes. Setting it to False could greatly speedup.
+#     with_stack=True
 trainer = L.Trainer(
     default_root_dir="output/",
     enable_progress_bar=wandb.run.disabled,
     max_epochs=C["epochs"],
     logger=wandb_logger,
     profiler='simple',
-    limit_train_batches=20,
+    limit_train_batches=10,
     limit_val_batches=10,
     benchmark=True,
     # num_sanity_val_steps=1,
@@ -98,11 +122,11 @@ logger.info("Starting training with first validation...")
 logger.info("Starting model fit...")
 trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 logger.info("Finished training. Dumping CUDA memory snapshot...")
-torch.cuda.memory._dump_snapshot(filename='output/memory_trace.pickle')
+# torch.cuda.memory._dump_snapshot(filename='output/memory_trace.pickle')
 # memory_artifact = wandb.Artifact("memory_trace", type="profiling")
 # memory_artifact.add_file("output/memory_trace.pickle")
 wandb.log_artifact('output/memory_trace.pickle', type="profiling", name=RUN_NAME + ".memory_trace.prof")
 
-logger.info("Starting final validation...")
-trainer.test(model=model, dataloaders=val_loader)
+# logger.info("Starting final validation...")
+# trainer.test(model=model, dataloaders=val_loader)
 logger.info("Finished training and testing. Goodbye.")
