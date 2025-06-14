675d8c8741f236b7c2d08078bda6961cfa4289fa
Milan Tresoor <milan.tresoor@gmail.com>
2025-06-14 19:30:06 +0200

    XR test_tinyhistory

--- model_cache_overview.py
+++ model_cache_overview.py
@@ -35,7 +35,6 @@ Flow_brown_normal_allC_BIG.h5
 Flow_seq_brown_normal_allC_BIG.h5
 R2_seq_normal_smC_rk4_40.h5
 R_seq_levy_smC_BIG.h5
-seq_brown-sig06_smC.h5
 seq_brown_smC-2-rk4_40.h5
 seq_brown_smC_BIG.h5
 seq_brown_smC_LONG.h5
@@ -52,11 +51,13 @@ test_cache_tinyhistory.h5
 test_cache_unflow.h5
 test_seq_normal_smC_rk4_40.h5
 test_tinyhistory.h5
-tinyhistory_seq_normal_smC_BIG.h5
 UnFlow_brown_normal_allC_BIG.h5
 UnFlow_brown_normal_smC_BIG.h5
 UnFlow_seq_brown_smC.h5""".split()
-
+archive = """
+tinyhistory_seq_normal_smC_BIG.h5
+seq_brown-sig06_smC.h5
+"""
 
 # def cache_exists(cache_name, cache_dir):
 #     if pd.isna(cache_name):
@@ -70,7 +71,7 @@ df['cache_exists'] = df['test_cache_name'].apply(lambda v: (v+'.h5') in caches)
 # Drop columns starting with 'test/'
 df = df.loc[:, ~df.columns.str.startswith('test/')]
 # Reorder columns: group_cols + [cache_col] + rest
-important_cols = ['cache_exists'] +group_cols + ["Name",  cache_col, 'test_cache_mode', "epoch"]
+important_cols = ['cache_exists'] +group_cols + ["Name",  cache_col, 'base_run', 'Created','test_cache_mode', "epoch"]
 other_cols = [col for col in df.columns if col not in important_cols]
 df = df[important_cols + other_cols]
 df
@@ -88,14 +89,16 @@ for group_keys, group_df in groups:
     for col, val in zip(group_cols, group_keys):
         print(f'  {col}: {val}')
     print('  Caches:')
-    for _, row in group_df.iterrows():
-        print(f"    {row[cache_col]} {'✅' if row['cache_exists'] else ''}")
+    for _, row in group_df.sort_values('Created').iterrows():
+        print(
+            f"     {'✅' if row['cache_exists'] else ''} {row[cache_col]}   from  base run '{row['base_run']}' [{row['test_cache_mode']}]"
+        )
 
 
 # %%
 # Create a pivot table summarizing available caches per group
 def summarize_caches(group):
-    available = group[group['cache_exists']][cache_col].tolist()
+    available = group[group['cache_exists']].sort_values('Created')[cache_col].tolist()
     if available:
         return '✅'+', ✅'.join(available)
     else:
